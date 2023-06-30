4e6734805bf27438db4bc71c7b4de318d26f0ba8
Wilhelm Liao <williaml.twcp@gmail.com>
2023-06-30 16:21:48 +0800

    WIP: add ContextBuilder()

---
+++ internal/contextBuilder.go
@@ -0,0 +1,45 @@
+package internal
+
+import (
+	"context"
+	"log"
+)
+
+type ContextBuilder struct {
+	ctx *Context
+}
+
+func NewContextBuilder() *ContextBuilder {
+	return &ContextBuilder{
+		ctx: new(Context),
+	}
+}
+
+func (b *ContextBuilder) ConsumerGroup(v string) *ContextBuilder {
+	b.ctx.ConsumerGroup = v
+	return b
+}
+
+func (b *ContextBuilder) ConsumerName(v string) *ContextBuilder {
+	b.ctx.ConsumerName = v
+	return b
+}
+
+func (b *ContextBuilder) Context(v context.Context) *ContextBuilder {
+	b.ctx.context = v
+	return b
+}
+
+func (b *ContextBuilder) Logger(v *log.Logger) *ContextBuilder {
+	b.ctx.logger = v
+	return b
+}
+
+func (b *ContextBuilder) InvalidMessageHandler(v MessageHandler) *ContextBuilder {
+	b.ctx.invalidMessageHandler = v
+	return b
+}
+
+func (b *ContextBuilder) Build() *Context {
+	return b.ctx
+}
